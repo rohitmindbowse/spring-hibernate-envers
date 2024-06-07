15682782c7b9d3a203ef2d6d0a7b07aae40dc834
Rohit Kumar <119486598+rohitmindbowse@users.noreply.github.com>
2024-06-07 10:23:00 +0530

    Add files via upload

---
+++ src/main/java/com/ehcache/demo/HibernateEnversApplication.java
@@ -0,0 +1,13 @@
+package com.ehcache.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEnversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehcache/demo/HibernateEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehcache.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEnversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
